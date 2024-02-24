85443ff353ec1eba62aa97f8d282ec85c3280c63
Gusman83 <gusmankara@gmail.com>
2024-02-24 16:44:50 +0700

    update lagi

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('hello world');
+    res.end('selamat siang dan selamat makan siang');
 });
 
 server.listen(port, hostname, () => {
